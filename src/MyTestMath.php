0a55ed0a31528ac28e19d2b77b0439119c2fec92
arlex2305k <aap@digsol.ru>
2025-11-21 09:29:55 +0300

    Initial version

---
+++ src/MyTestMath.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+namespace Arlex2305k\Test;
+
+class MyTestMath
+{
+    /**
+     * Рекурсивный метод для расчета факториала числа
+     * @param int $n
+     * @return int
+     */
+    public static function factorial(int $n)
+    {
+        if ($n < 0) {
+            throw new \InvalidArgumentException('Факториал не определен для отрицательных чисел');
+        }
+        
+        if ($n == 0 || $n == 1) {
+            return 1;
+        }
+        
+        return $n * self::factorial($n - 1);
+    }
+}
---
+++ test.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Arlex2305k\Test\MyTestMath;
+
+echo MyTestMath::factorial(10) . PHP_EOL;
